927f082fa79a5beace2dfd57cc406889704387a2
Juninho <juninhopeter1@gmail.com>
2024-08-19 17:02:11 +0100

    feat(user): added new controller for user

---
+++ CICDPROJECT/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICDPROJECT.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            return Ok("Welcome to Juninho world of tech");
+        }
+    }
+}
