e10caef22ff9b0d104c5b7443b81393e8c19a3b7
Juninho <juninhopeter1@gmail.com>
2024-08-19 17:57:55 +0100

    added new controller for booking

---
+++ CICDPROJECT/Controllers/BookingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICDPROJECT.Controllers
+{
+    [Route("api/booking")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Booking(string name)
+        {
+            return CreatedAtAction(nameof(Booking), new 
+            {
+                message = $"Hello, {name}, your booking time is: {DateTime.Now}"
+            });
+        }
+    }
+}
